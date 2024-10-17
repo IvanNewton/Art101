ccfd8e81d5c1ae1bc80e860d31e6aa6db6e4994b
IvanNewton <ivannewton4286@gmail.com>
2024-10-17 12:19:04 -0700

    did lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - lab 4: Pseudocode for wordle
+// Author: Ivan Newton
+// Date: 10/17/2024
+
+
+//wordle pseudocode
+    //Set up the game grid.
+    //Randomly select a target word.
+    //Allow the player to input guesses.
+        //Check if each guess is correct, providing feedback on letter matches and positions.
+        //Track attempts until the player successfully guesses the target word.
+
 
 // Constants
 
