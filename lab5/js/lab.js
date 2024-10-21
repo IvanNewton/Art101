4e6e1adf28ad5fcb2c0cc1fab1f47bd64e1e3fdc
IvanNewton <ivannewton4286@gmail.com>
2024-10-21 11:06:57 -0700

    worked on/finished lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,11 +1,14 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - experiment with basic JavaScript data types and variables
+// Author: Ivan Newton
+// Date: 10/21/2024
 
 // Constants
-
+  make = "toyota";
+  model = "Prius C";
+  color = "white";
+  year = 2015;
 // Functions
-
+age = 2024 - year;
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
@@ -19,3 +22,8 @@ function main() {
 
 // let's get this party started
 main();
+document.writeln("make: "+ make + "<br>");
+document.writeln("modle: "+ modle + "<br>");
+document.writeln("color: "+ color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age:" + age + "<br>");
