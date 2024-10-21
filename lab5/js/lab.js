8d13755b32820bd7ae6e9118ff511eba0cf10122
IvanNewton <ivannewton4286@gmail.com>
2024-10-21 11:26:55 -0700

    continued working on lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -8,22 +8,10 @@
   color = "white";
   year = 2015;
 // Functions
-age = 2024 - year;
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+  age = 2024 - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
-document.writeln("make: "+ make + "<br>");
-document.writeln("modle: "+ modle + "<br>");
-document.writeln("color: "+ color + "<br>");
-document.writeln("year: " + year + "<br>");
-document.writeln("age:" + age + "<br>");
+document.writeln("make: "+ make +"\n");
+document.writeln("modle: "+ modle +"\n");
+document.writeln("color: "+ color +"\n");
+document.writeln("year: "+ year +"\n");
+document.writeln("age:"+ age +"\n");
