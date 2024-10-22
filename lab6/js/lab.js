420dc41a3414cc466e479a4d9060e8b0b6e226a1
IvanNewton <ivannewton4286@gmail.com>
2024-10-22 16:16:25 -0700

    Finished lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Experiment with JS arrays and objects
+// Author: Ivan Newton
+// Date: 10/22/2024
 
-// Constants
+// variables
+myTransport = ["toyota Prius C ", "bus ", "rides from friends ", "feet."]
 
-// Functions
+//object for main ride
+myMainRide = {
+  make: "toyota",
+  model: "Prius C",
+  color: "white",
+  year: 2015,
+  //fuction for age
+  age: function(){
+      return 2022 - this.year;
+  }
+
+}
+//output
+document.writeln("Kinds of Transportation I use: ", myTransport, "</br>")
+//allows us to write an object into the document
+document.writeln("My main ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "<pre>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
