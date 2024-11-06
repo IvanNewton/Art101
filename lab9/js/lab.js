2ebfbb910e17c7829f9c439e7412c3b95d766455
IvanNewton <ivannewton4286@gmail.com>
2024-11-05 17:42:47 -0800

    LAB 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -13,6 +13,6 @@
     // add a click listener to the challenge button
 $("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
+    $("#challenge").toggleClass("Make Special");
     $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 });
